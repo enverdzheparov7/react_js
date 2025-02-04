dfc26d957022353d0a4c8705c4fc62b01971adec
enverdzheparov7 <dzheparovei.23@gmail.com>
2025-02-04 12:43:00 +0300

    9 задач

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+	return <div class="new_class">
+<p>I’m beginning to feel like a Rap God, Rap God</p>
+<p>All my people from the front to the back nod, back nod</p>
+<p>Now who thinks their arms are long enough to slapbox, slapbox?</p>
+<p>They said I rap like a robot, so call me Rapbot</p>
+<ul>
+<li>I’m beginning to feel like a Rap God, Rap God</li>
+<li>All my people from the front to the back nod, back nod</li>
+<li>Now who thinks their arms are long enough to slapbox, slapbox?</li>
+<li>They said I rap like a robot, so call me Rapbot</li>
+<li>All my people from the front to the back nod, back nod</li>
+<li>Now who thinks their arms are long enough to slapbox, slapbox?</li>
+<li>They said I rap like a robot, so call me Rapbot</li>
+<li>All my people from the front to the back nod, back nod</li>
+<li>Now who thinks their arms are long enough to slapbox, slapbox?</li>
+<li>They said I rap like a robot, so call me Rapbot</li>
+</ul>
+	</div>;
+}
+
+export default App;
